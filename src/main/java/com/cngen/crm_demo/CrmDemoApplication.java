a2b485afd517e85a67d075122c0ce49bbc7108b3
loinv <loinguyen.work1997@gmail.com>
2024-12-17 22:19:15 +0700

    initilize

---
+++ src/main/java/com/cngen/crm_demo/CrmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cngen.crm_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cngen/crm_demo/CrmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cngen.crm_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
